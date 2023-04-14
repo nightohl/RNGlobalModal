2b2a855c46cc735d8cc6efc440875971d45d6125
nightohl <haneojin@gmail.com>
2023-04-14 11:56:18 +0900

    test: ModalA 팝업 중첩 예시코드 적용

--- src/screens/A.tsx
+++ src/screens/A.tsx
@@ -1,13 +1,26 @@
 import React from 'react';
-import {SafeAreaView, Text} from 'react-native';
+import {Button, SafeAreaView, Text} from 'react-native';
 import {NavigateButton} from '../components/NavigateButton';
+import {useModalA} from '../features/ModalA/hooks/useModalA';
 
 export const A = () => {
+  const {openModal} = useModalA({
+    title: 'Some Error',
+    description: 'blah blah',
+  });
+
+  const multipleModals = () => {
+    openModal();
+    setTimeout(() => openModal(), 500);
+    setTimeout(() => openModal(), 1000);
+    setTimeout(() => openModal(), 1500);
+  };
+
   return (
     <SafeAreaView>
       <Text>A</Text>
       <NavigateButton screen="B" />
-      <NavigateButton screen="GlobalModal" />
+      <Button title="GlobalModal" onPress={multipleModals} />
     </SafeAreaView>
   );
 };
